64b437d95f01fb5370cce3580e074b1471542e6c
Juan cruz Bonadeo <juancruzbonadeo04@gmail.com>
2023-05-24 09:22:11 -0300

    primer preentrega

--- BackendCoder/server.js
+++ BackendCoder/server.js
@@ -1,54 +1,14 @@
 import express from "express";
-
-import ProductManager from "./index.js";
+import routerProductos from "./routes/products.router.js";
+import routerCarrito from "./routes/carts.router.js";
 
 const app = express();
-
-const PM = new ProductManager();
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-function getLimitedArray(array, count) {
-  return array.slice(0, count);
-}
-
-
-app.get("/products", (req, res) => {
-  PM.getProducts().then((products) => {
-    const limit = parseInt(req.query.limit);
-
-    if (limit && !Number.isInteger(limit)) {
-      res
-        .status(400)
-        .json({
-          error:
-            "El parámetro limit debe ser un numero mayor a 0 y debe ser un entero.",
-        });
-      return;
-    }
-
-    res.send(limit > 0 ? getLimitedArray(products, limit) : products);
-  });
-});
-
-app.get('/products/:pid', async (req, res) => {
-  const productId = req.params.pid;
-  const product = await PM.getProductsById(productId);
-  return res.json(product);
-});
-
-app.delete('/products/:pid', async (req, res) => {
-  await PM.deleteProduct(req.params.pid);
-  const newProducts = await PM.getProducts();
-  
-  return res.json(newProducts);
-});
-
-
+app.use("/api/products", routerProductos);
+app.use("/api/carrito", routerCarrito);
 
 app.listen(8080, () => {
   console.log("Servidor iniciado en el puerto 8080");
